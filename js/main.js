3db6f9144cf3fbcc09b3a273f1ea3533c6edefda
Mohamed Ben Hamza <mohamedbenhamza90@gmail.com>
2021-10-10 00:01:16 +0100

    sign up page responsive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var toggle = document.querySelector('.toggle')
+console.log(toggle);
+const navList=document.querySelectorAll('.nav-list')
+
+
+toggle.addEventListener('click',function(){
+    navList.forEach(element => {
+        element.classList.toggle('hide')
+    });
+})
---
+++ sign_in.php
@@ -0,0 +1,3 @@
+<?php
+
+include "sign_in.phtml";
